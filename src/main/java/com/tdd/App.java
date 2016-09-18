6c582fe58f4b969134e66a6e0171fa57aa768241
tdd487 <168700784@qq.com>
2016-09-18 18:21:30 +0800

    Initial commit

---
+++ src/main/java/com/tdd/App.java
@@ -0,0 +1,33 @@
+package com.tdd;
+
+import com.tdd.model.CountryEntity;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg = new Configuration().configure();
+
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        Session session = null;
+
+        session = factory.openSession();
+
+        session.beginTransaction();
+        System.out.println(cfg);
+        String sql = "FROM CountryEntity";
+        Query query = session.createQuery(sql);
+        System.out.println( query.list());
+
+    }
+}
